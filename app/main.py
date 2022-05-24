256dcb1b952866db43e819d502f45f4cdc32f664
Aumit Leon <aleon@indigoag.com>
2022-05-24 19:18:22 -0400

    update benchmarks

--- app/main.py
+++ app/main.py
@@ -8,10 +8,18 @@ app = FastAPI()
 @app.get("/sync-loadtest")
 def synchronous_route():
     time.sleep(1)
+
+    # Grab some amount of memory
+    bytearray(512000000)
     return {"message": "Synchronous Hello World!"}
 
 
 @app.get("/async-loadtest")
-def synchronous_route():
-    asyncio.sleep(1)
-    return {"message": "AsynchronousHello World!"}
+async def asynchronous_route():
+    await asyncio.sleep(1)
+    start_time= time.time()
+    # Emulate large objects in memory
+    bytearray(512000000)
+    end_time= time.time()
+    print(f'time to allocate bytearray {end_time - start_time}')
+    return {"message": "Asynchronous Hello World!"}
--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -4,3 +4,8 @@ class CustomoUvicornWorker(UvicornWorker):
     CONFIG_KWARGS = {"proxy_headers": True, "forwarded_allow_ips": "*"}
 
 worker_class = "gunicorn_conf.CustomoUvicornWorker"
+
+workers = 4
+
+
+#max_requests = 500
